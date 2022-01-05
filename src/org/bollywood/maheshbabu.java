5309e9802881879b4e9eae4b3fdd6274f6c66d7f
velagalamohan <velagalamohan@gmail.com>
2022-01-05 21:36:47 +0530

    add mani

--- src/org/bollywood/maheshbabu.java
+++ src/org/bollywood/maheshbabu.java
@@ -1,5 +1,11 @@
 package org.bollywood;
 
 public class maheshbabu {
-
+	public void Mahi() {
+System.out.println("mahesh");
+	}
+public static void main(String[] args) {
+	maheshbabu b = new maheshbabu();
+	b.Mahi();
+}
 }
